af55cec573d4849bd53e6d1d6a4c30d8cdb3605f
oldmanCoding <sanhquan68@gmail.com>
2022-10-10 22:14:04 +0700

    While loop to get parent element

--- Form-Validation/formValidation.js
+++ Form-Validation/formValidation.js
@@ -3,31 +3,81 @@ function Validator (options) {
     // Get element of form need to be validated
     var formElement = document.querySelector(options.form);
     var selectorRules = {};
+
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement
+                
+            }
+            element = element.parentElement;
+        }
+
+    }
+    
     
     function validate(inputElement, rule, errorSelector) {
-        var errorElement =  inputElement.parentElement.querySelector(errorSelector);
+        var errorElement =  getParent(inputElement, options.formGroupSelector).querySelector(errorSelector);
         var rulesSelect = selectorRules[rule.selector];
 
         for (var i=0; i < rulesSelect.length; i++) {
             var errorMessage = rulesSelect[i](inputElement.value)
-            if (errorMessage) {
-                break;
-            }   
+            if (errorMessage) break;
         }
 
         if (errorMessage) {
             errorElement.innerText = errorMessage;
             inputElement.parentElement.classList.add('invalid');
+            return false;
         } else {
             errorElement.innerText = '';
             inputElement.parentElement.classList.remove('invalid');
+            return true;
         }
+        
         
     }
 
     if (formElement) {
+        
+
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+            var isFormValid = true;
+
+            options.rules.forEach(function (rule) {
+                var inputElement = document.querySelector(rule.selector);
+                var errorSelector = options.errorSelector;
+                var isValid = validate(inputElement, rule, errorSelector);
+
+                if (!isValid) {
+                    isFormValid = false
+                }
+                
+            })
+            
+            
+            if (isFormValid) {
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = document.querySelectorAll('input[name]:not([disabled])');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) { 
+                        values[input.name] = input.value;                
+                        return values;
+                    }, {})
+
+                    options.onSubmit(formValues);
+                }
+                else {
+                    formElement.submit()
+                }
+            }
+           
+
+        }
+
         options.rules.forEach(function (rule) {
             // Save rule for each input element
+
             if (Array.isArray(selectorRules[rule.selector])) {
                 selectorRules[rule.selector].push(rule.test)
             } else {
@@ -51,7 +101,6 @@ function Validator (options) {
                 }
             }
         })
-        console.log(selectorRules)
     } 
 }
 
