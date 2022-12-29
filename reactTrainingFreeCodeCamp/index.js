6845d69910ba209ebdca307f112b77ca7fd4c60a
Tuongnm <ng.manhtuong@gmail.com>
2022-12-29 11:30:41 +0700

    append vanilla js - imperative

--- reactTrainingFreeCodeCamp/index.js
+++ reactTrainingFreeCodeCamp/index.js
@@ -1,1 +1,6 @@
-ReactDom.render(<h1>Hello, React coming</h1>, document.getElementById(root))
+const h1 = document.createElement('h1');
+h1.textContent = "This is vanilla js" ; 
+
+h1.className = "header";
+
+document.getElementById("root").appendChild(h1);
