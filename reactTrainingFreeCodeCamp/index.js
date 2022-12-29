608bd72c6888bbb8b15b5367845a2d6dbd72525c
Tuongnm <ng.manhtuong@gmail.com>
2022-12-29 17:17:51 +0700

    render function

--- reactTrainingFreeCodeCamp/index.js
+++ reactTrainingFreeCodeCamp/index.js
@@ -11,15 +11,57 @@ console.log(h1);
     
 // )
 
+// const element = (
+//     <nav>
+//         <h1>React training</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About us</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
 const element = (
-    <nav>
-        <h1>React training</h1>
-        <ul>
-            <li>Pricing</li>
-            <li>About us</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+    <div>
+        <h1>My react website </h1>
+        <h2>Reason i love react</h2>
+        <ol>
+            <li>Reuseable</li>
+            <li>Fast</li>
+            <li>Composable</li>
+        </ol>
+    </div>
 )
-    
-ReactDOM.render(element, document.getElementById("root")) 
+
+const page = (
+    <div>
+        <img src ="./logo-react.png" width = "40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was firt original in 2013</li>
+            <li>Has 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Power thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+) 
+
+function temproraryPage() {
+    return (
+        <div>
+        <img src ="./logo-react.png" width = "40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was firt original in 2013</li>
+            <li>Has 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Power thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+    )
+}
+
+// document.getElementById("root").append(JSON.stringify(element))
+
+ReactDOM.render(temproraryPage(), document.getElementById("root")) 
