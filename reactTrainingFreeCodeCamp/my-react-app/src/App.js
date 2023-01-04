346dfd9ba90ecd2a89707e892fd655e665da7d2e
Tuongnm <ng.manhtuong@gmail.com>
2023-01-04 10:29:47 +0700

    feat: minor change css style facebook square

--- reactTrainingFreeCodeCamp/my-react-app/src/App.js
+++ reactTrainingFreeCodeCamp/my-react-app/src/App.js
@@ -7,10 +7,10 @@ import About from './components/About';
 import Footer from './components/Footer';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faLinkedin, faGithub, faTwitter, faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub, faTwitter, faFacebookSquare, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faLinkedin, faGithub, faEnvelope, faTwitter, faFacebookSquare, faInstagram)
+library.add(faLinkedin, faGithub, faEnvelope, faTwitter, faFacebookSquare, faFacebookF, faInstagram)
 
 function App() {
   return (
--- reactTrainingFreeCodeCamp/my-react-app/src/components/Footer.js
+++ reactTrainingFreeCodeCamp/my-react-app/src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className = "footer">
             <FontAwesomeIcon icon="fa-brands fa-twitter" className='footer__icon' />
-            <FontAwesomeIcon icon="fa-brands fa-square-facebook" className='footer__icon' />
+            <FontAwesomeIcon icon="fa-brands fa-facebook-f" className='footer__icon' />
             <FontAwesomeIcon icon="fa-brands fa-github" className='footer__icon' />
             <FontAwesomeIcon icon="fa-brands fa-instagram" className='footer__icon' />
         </div>
