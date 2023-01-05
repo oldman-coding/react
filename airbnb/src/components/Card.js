64a1ae4328b6beff9776aedb234566ed5b2c599c
Tuongnm <ng.manhtuong@gmail.com>
2023-01-05 17:08:51 +0700

    style card badge absolute

--- airbnb/src/components/Card.js
+++ airbnb/src/components/Card.js
@@ -1,4 +1,4 @@
-// import zera from "%PUBLIC_URL%/images/";
+import zera from "../images/zaferes.jpeg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
@@ -8,22 +8,24 @@ export default function Card({data}) {
         <div className="card-wrapper">
             {
                 data.map(item => {
-                    return <div className="card">
-                    <img src= {`%PUBLIC_URL%/images/${item.img}`} className="card__image" />
-                    <div className="card__star">
-                        <FontAwesomeIcon icon="fa-solid fa-star" className="card__star-icon" />
-                        {item.rating && <span className="card__star-text">{item.rating} </span>}
-                        <span className="card__star-text">({item.qty}) - </span>
-                        <span className="card__star-text">{item.country}</span>
-                    </div>
-                    <p className="card__course">
-                        {item.course} 
-                    </p>
-                    <div className="card__price">
-                        <p>From {item.price}</p>
-                        <span> / person</span>
-                    </div>
-                </div>
+                    return <div className="card" key = {item.id}>
+                            <div className="card__badge">SOLD OUT</div>
+                            {/* <img src= {`%PUBLIC_URL%/images/${item.img}`} className="card__image" /> */}
+                            <img src= {zera} className="card__image" />
+                            <div className="card__star">
+                                <FontAwesomeIcon icon="fa-solid fa-star" className="card__star-icon" />
+                                {item.rating && <span className="card__star-text">{item.rating} </span>}
+                                <span className="card__star-text">({item.qty}) - </span>
+                                <span className="card__star-text">{item.country}</span>
+                            </div>
+                            <p className="card__course">
+                                {item.course} 
+                            </p>
+                            <div className="card__price">
+                                <p>From {item.price}</p>
+                                <span> / person</span>
+                            </div>
+                        </div>
                 })
             }
         </div>
--- airbnb/src/data/data.js
+++ airbnb/src/data/data.js
@@ -1,27 +1,33 @@
 export default [
     {   
+        id: 1,
         img: 'zaferes.jpeg', 
         rating: '5.0', 
         qty: '6', 
         country: 'USA', 
         course: 'Life lessons with Katie Zaferes',
-        price: "$139"
+        price: "$139", 
+        spot: 0, 
     },
     {   
+        id: 2,
         img: 'zaferes.jpeg', 
         rating: '5.0', 
         qty: '6', 
         country: 'USA', 
         course: 'Life lessons with Katie Zaferes',
-        price: "$139"
+        price: "$139",
+        spot: 5, 
     }, 
     {   
+        id: 3,
         img: 'zaferes.jpeg', 
         rating: '5.0', 
         qty: '6', 
         country: 'USA', 
         course: 'Life lessons with Katie Zaferes',
-        price: "$139"
+        price: "$139", 
+        spot: 10, 
     } 
 ]
 
