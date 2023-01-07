999746c790672fc2a92c6bdfa15a73c218014af5
Tuongnm <ng.manhtuong@gmail.com>
2023-01-07 14:58:19 +0700

    style absolute center

--- meme_generator/src/components/Form.js
+++ meme_generator/src/components/Form.js
@@ -1,12 +1,20 @@
+import memeimg from '../images/memeimg.png';
+
 export default function Form() {
     return (
         <form className='form'>
             <div className = "form__data">
-                <input className = "form__input" placeholder="take me"></input>
-                <input className = "form__input" placeholder ='input'></input>
+                <input className = "form__input" placeholder="Shut up"></input>
+                <input className = "form__input" placeholder ='and take my money'></input>
             </div>
 
             <button className="form__submit" type = 'submit'>Get a new meme image</button>
+            <div className="form__generator">
+                <img className="form__img" src={memeimg} />
+                <div className = "form__text form__text--top">SHUT UP</div>
+                <div className = "form__text form__text--bottom">AND TAKE MY MONEY</div>
+            </div>
+            
         </form>
     )
 }
