652dd82a8ca4296b3e82cd7da5fdcf22e6900d5e
Tuongnm <ng.manhtuong@gmail.com>
2023-01-10 20:33:33 +0700

    refactor pass props to child component

--- meme_generator/src/App.js
+++ meme_generator/src/App.js
@@ -6,13 +6,19 @@ import Counter from './components/Counter';
 import AddItem from './components/AddItem';
 import Contact from './components/Contact';
 import Boxes from './components/Boxes';
+import { useState } from 'react';
 
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import dataBoxes from './data/dataBoxes'
+
 
 library.add(faStar,)
 
 function App() {
+  const [boxes, setBoxes] = useState (dataBoxes)
+  
+  const boxesElements = boxes.map(box => <Boxes key={box.id} {...box} />)
   return (
     <div className="App">
       <Navbar />
@@ -20,7 +26,7 @@ function App() {
       <Counter />
       <AddItem />
       <Contact />
-      <Boxes darkMode ={true} />
+      {boxesElements}
     </div>
   );
 }
--- meme_generator/src/components/Boxes.js
+++ meme_generator/src/components/Boxes.js
@@ -1,17 +1,13 @@
 import { useState } from "react";
-import dataBoxes from '../data/dataBoxes'
+
 import '../boxes.css'
-import { faBlackboard } from "@fortawesome/free-solid-svg-icons";
 
 export default function Boxes(props) {
-    console.log(props)
-    const [boxes, setBoxes] = useState (dataBoxes)
+    
     const styles = {
-        backgroundColor: props.darkMode ? "#222222" : "#cccccc"
+        backgroundColor: props.on ? "#222222" : "#cccccc"
     }
-
-    const boxesElements = boxes.map(box => <div style= {styles} className="box__item" key={box.id}>{box.id}</div>)
     return (
-        <div  className="box">{boxesElements}</div>
+        <div  style = {styles} className="box">{props.id}</div>
     )
 }
